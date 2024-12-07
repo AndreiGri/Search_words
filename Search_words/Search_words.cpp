7e17ab82f6f77794fe6aed08e55ec23ca0ec8bfe
Andrei_Gridin <andreigridin918@gmail.com>
2024-12-08 03:45:51 +0500

    Добавьте файлы проекта.

---
+++ Search_words/Search_words.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    system("chcp 1251>nul");                           // Изменение кодировки кансоли
+    system("color 80");                                // Изменяем цвет консоли и текста
+    ifstream word;                                     // Создаём входной поток
+    word.open("words.txt");                            // Открываем файл
+    if (!word.is_open())                               // Проверяем открытие файла
+    {
+        cout << " Не удаётся открыть файл!" << endl;
+        return 1;                                      // Обработка ошибки
+    }
+
+    
+    word.close();                                      // Закрываем файл
+    system("pause>nul");                               // Задержка консоли окна
+    return 0;
+}
